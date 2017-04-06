5da467141bc3b052b795cf5d19b53f75ff8235bf
Michael Lee <selicas@gmail.com>
2017-04-05 17:17:13 -0700

    updated Haspi consolidate function
    completed haspi consolidate function.

--- HASPI consolidate.R
+++ HASPI consolidate.R
@@ -2,5 +2,21 @@
 
 # This code takes individual HASPI results and consolidates them into a master file
 
-setwd("E:/Google Drive/UW_subject_data")
-list.files(path=".")
+setwd("E:/Google Drive/Project AD_ Hearing Aids and SNR/SIN/subject_data/Results")
+filelist <- list.files(path=".",pattern="*analysis_Hagerman*")
+filelist_length <- length(filelist)
+
+for (i in 1:filelist_length){
+
+current_file_data <- read.csv(filelist[i])
+if (i==1){
+    master_file_data <- current_file_data
+    
+} else {
+    master_file_data <- rbind(master_file_data,current_file_data)
+}
+
+}
+
+paste("HASPI_data",Sys.Date(),".csv")
+write.csv(master_file_data,paste("HASPI_data",Sys.Date(),".csv",sep=""),)
