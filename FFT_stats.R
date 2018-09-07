0c76a88f9239c772a106f3e24a1fae6c7683568f
Michael Lee <selicas@gmail.com>
2018-09-07 11:01:56 -0700

    Update library

---
+++ FFT_stats.R
@@ -0,0 +1,12 @@
+setwd('E:/Google Drive/Project AE_SNR EEG ERP/Data/FFT/PSD')
+
+#Load libraries
+library(dplyr)
+library(ggplot2)
+library(ggthemes)
+library(corrplot)
+library(corrgram)
+library(usdm)
+library(car)
+library(R.matlab)
+
