a55f90131d90af330ecec5c4a5b71ba71659658c
Michael Lee <selicas@gmail.com>
2018-09-11 13:10:37 -0700

    Added input of Hagerman test ID and output file

--- HASPI consolidate.R
+++ HASPI consolidate.R
@@ -2,8 +2,15 @@
 
 # This code takes individual HASPI results and consolidates them into a master file
 
+library(shiny)
+
 setwd("E:/Google Drive/Project AD_ Hearing Aids and SNR/SIN/subject_data/Results")
-filelist <- list.files(path=".",pattern="*analysis_Hagerman*")
+
+# Select Hagerman test results to consolidate. Do not include Parentheses in the selection input. 
+
+h_test <- readline(prompt="Enter name of one Hagerman test. Parentheses surround each choice : (Aided, ISTS) , (Aided, SPSHN) , (Unaided, ISTS) , (Unaided SPSHN) :   ")
+
+filelist <- list.files(path=".",h_test="*analysis_Hagerman*")
 filelist_length <- length(filelist)
 
 for (i in 1:filelist_length){
@@ -19,4 +26,4 @@ if (i==1){
 }
 
 paste("HASPI_data",Sys.Date(),".csv")
-write.csv(master_file_data,paste("HASPI_data",Sys.Date(),".csv",sep=""),)
+write.csv(master_file_data,paste("HASPI ",h_test,' ',Sys.Date(),".csv",sep=""),)
